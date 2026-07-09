2e438da2ad9298a694fa05597f3801142c045a60
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2026-07-09 15:31:07 -0400

    Se termina por completo la funcionalidad del crud de productos en el frontend. Incluyendo la actualización de la disponibilidad de los productos productos con patch.

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Form, redirect, useNavigate, type ActionFunctionArgs } from "react-router-dom"
+import { Form, redirect, useFetcher, useNavigate, type ActionFunctionArgs } from "react-router-dom"
 import { formatCurrency } from "../helpers"
 import type { Product } from "../types"
 import { deleteProduct } from "../services/ProductService"
@@ -15,7 +15,7 @@ export async function action({ params} : ActionFunctionArgs) {
 }
 
 export default function ProductDetails({ product } : ProductDetailsProps) {
-
+    const fetcher = useFetcher()
     const navigate = useNavigate()
     const isAvailable = product.availability
 
@@ -28,7 +28,16 @@ export default function ProductDetails({ product } : ProductDetailsProps) {
                 {formatCurrency(product.price)}
             </td>
             <td className="p-3 text-lg text-gray-800">
-                {isAvailable ? 'Disponible' : 'No disponible'}
+                <fetcher.Form method='POST'>
+                    <button
+                        type="submit"
+                        name="id"
+                        value={product.id}
+                        className={`${isAvailable ? 'text-black': 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
+                    >
+                        {isAvailable ? 'Disponible' : 'No disponible'}
+                    </button>
+                </fetcher.Form>
             </td>
             <td className="p-3 text-lg text-gray-800 ">
              <div className="flex gap-2 items-center">
