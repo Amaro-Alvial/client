cd80e28c4909389f17cee1794d95d96ed3ff6655
Amaro-Alvial-Student <a.alvialbravo@uandresbello.edu>
2026-07-09 15:08:35 -0400

    Se termina de dar la funcionalidad a la eliminación de productos.

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,9 +1,17 @@
-import { useNavigate } from "react-router-dom"
+import { Form, redirect, useNavigate, type ActionFunctionArgs } from "react-router-dom"
 import { formatCurrency } from "../helpers"
 import type { Product } from "../types"
+import { deleteProduct } from "../services/ProductService"
 
 type ProductDetailsProps = {
     product: Product
+}
+
+export async function action({ params} : ActionFunctionArgs) {
+    if (params.id !== undefined){
+        await deleteProduct(+params.id)
+        return redirect('/')
+    }
 }
 
 export default function ProductDetails({ product } : ProductDetailsProps) {
@@ -26,8 +34,24 @@ export default function ProductDetails({ product } : ProductDetailsProps) {
              <div className="flex gap-2 items-center">
                 <button
                     onClick={() => navigate(`/productos/${product.id}/editar`)}
-                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:bg-indigo-500"
+                    className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:bg-indigo-500 hover:cursor-pointer"
                 >Editar</button>
+                <Form
+                    className="w-full"
+                    method='POST'
+                    action={`productos/${product.id}/eliminar`}
+                    onSubmit={(e) => {
+                        if( !confirm(`¿Está seguro de que quiere eliminar ${product.name}?`)){
+                            e.preventDefault()
+                        }
+                    }}
+                >
+                    <input
+                        type='submit'
+                        value='eliminar'
+                        className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center hover:bg-red-500 hover:cursor-pointer"
+                    />
+                </Form>
              </div>
             </td>
         </tr> 
--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -1,7 +1,8 @@
 import { Link, Form, useActionData, redirect, type ActionFunctionArgs, type LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct, getProductById, updateProduct } from "../services/ProductService";
+import { getProductById, updateProduct } from "../services/ProductService";
 import type { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader({params} : LoaderFunctionArgs){
     if(params.id !== undefined){
@@ -54,35 +55,10 @@ export default function EditProduct() {
                 className="mt-10" 
                 method='POST'
             >
-            
-                <div className="mb-4">
-                    <label
-                        className="text-gray-800"
-                        htmlFor="name"
-                    >Nombre Producto:</label>
-                    <input 
-                        id="name"
-                        type="text"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Nombre del Producto"
-                        name="name"
-                        defaultValue={product.name}
-                    />
-                </div>
-                <div className="mb-4">
-                    <label
-                        className="text-gray-800"
-                        htmlFor="price"
-                    >Precio:</label>
-                    <input 
-                        id="price"
-                        type="number"
-                        className="mt-2 block w-full p-3 bg-gray-50"
-                        placeholder="Precio Producto. ej. 200, 300"
-                        name="price"
-                        defaultValue={product.price}
-                    />
-                </div>
+                <ProductForm
+                    product={product}
+                />
+
                 <div className="mb-4">
                     <label
                         className="text-gray-800"
@@ -102,7 +78,7 @@ export default function EditProduct() {
                 <input
                     type="submit"
                     className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-                    value="Registrar Producto"
+                    value="Guardar cambios"
                 />
             </Form>
         </>
